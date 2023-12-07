10199f0cb35b095f785256b3e95bd52b9cbaa45c
Edward D.M. Wilson <edw@sdf.org>
2023-12-07 15:21:39 +0000

    Process and echo keypresses, quit on C-q

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
-use std::io::Read;
+use termion::raw::IntoRawMode;
+use termion::async_stdin;
+use std::io::{Read, Write, stdout};
+use std::thread;
+use std::time::Duration;
 
 fn main() -> Result<(), std::io::Error> {
-    let mut line = String::new();
-    let c = std::io::stdin().read_to_string(&mut line)?;
-    dbg!(line);
-    println!("No. bytes read: {:?}", c);
+    let stdout = stdout();
+    let mut stdout = stdout.lock().into_raw_mode()?;
+    let mut stdin = async_stdin().bytes();
+
+    write!(stdout,
+           "{}{}",
+           termion::clear::All,
+           termion::cursor::Goto(1, 1))?;
+
+    loop {
+        let b = stdin.next();
+        match b {
+            None => (),
+            Some(Ok(17)) => break, // Ctrl-q
+            Some(Ok(_)) => write!(stdout, "{:?}\n\r", b)?,
+            _ => break,
+        }
+        stdout.flush()?;
+
+        thread::sleep(Duration::from_millis(50));
+        stdout.flush()?;
+    }
+
     Ok(())
 }
