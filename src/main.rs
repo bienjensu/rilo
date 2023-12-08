a845c4aad8c71dded0450aeb84b282b534ab8d58
Edward D.M. Wilson <edw@sdf.org>
2023-12-08 13:30:41 +0000

    Implement input handling in function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use termion::raw::IntoRawMode;
+use termion::raw::{IntoRawMode, RawTerminal};
 use termion::async_stdin;
-use std::io::{Read, Write, stdout};
+use termion::AsyncReader;
+use std::io::{Read, Write, Bytes, stdout, StdoutLock};
 use std::thread;
 use std::time::Duration;
+
+#[derive(PartialEq, Eq)]
+enum Command {
+    Quit,
+    Print,
+}
+
+#[derive(PartialEq, Eq)]
+struct Action {
+    command: Command,
+    character: Option<u8>,
+}
 
 fn main() -> Result<(), std::io::Error> {
     let stdout = stdout();
@@ -15,13 +28,16 @@ fn main() -> Result<(), std::io::Error> {
            termion::cursor::Goto(1, 1))?;
 
     loop {
-        let b = stdin.next();
-        match b {
-            None => (),
-            Some(Ok(17)) => break, // Ctrl-q
-            Some(Ok(_)) => write!(stdout, "{:?}\n\r", b)?,
-            _ => break,
+        let commands = handle_input(&mut stdin);
+
+        if commands.contains(&Action{command: Command::Quit, character: None}) {
+            break
         }
+
+        commands.into_iter()
+            .filter(|x| x.command == Command::Print)
+            .for_each(|x: Action | write!(stdout, "{:?}\r\n", x.character).unwrap());
+
         stdout.flush()?;
 
         thread::sleep(Duration::from_millis(50));
@@ -29,4 +45,18 @@ fn main() -> Result<(), std::io::Error> {
     }
 
     Ok(())
+}
+
+fn handle_input(stdin: &mut Bytes<AsyncReader>) -> Vec<Action> {
+    let mut out: Vec<Action> = vec![];
+
+    for (_, byte) in stdin.enumerate() {
+        let b = byte.unwrap();
+        match b {
+            b'q' => out.push(Action{command: Command::Quit, character: None}),
+            _    => out.push(Action{command: Command::Print, character: Some(b)}),
+        };
+    }
+
+    out
 }
